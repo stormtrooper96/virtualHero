ef70fa866e68b24015796b7a63670e6a35ea3912
licantropusdraconis <licantropusdraconis@gmail.com>
2018-09-01 15:48:40 -0400

    se agregan elementos para usuario y login basicos 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,34 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
-    skip_before_action :authorize_request, only: :create
+    # skip_before_action :authorize_request, only: :create
     # POST /signup
     # return authenticated token upon signup
     def create
-      user = User.create!(user_params)
-      auth_token = AuthenticateUser.new(user.email, user.password).call
-      response = { message: Message.account_created, auth_token: auth_token }
+      #si declaro con @ la variable está presente en toda la clase
+      @rol=Rol.find(params[:rol])
+      @user = User.create(user_params) #crear user, el permit es como un filtro, sólo permite unos elementos para evitar hackeo
+      auth_token = AuthenticateUser.new(@user.email, @user.password).call
+      @rol.users << @user #concatena a la lista de objetos que pertenecen a rol
+      response = { message: Message.account_created, auth_token: auth_token } #creacion de objeto con atributo message con un metodo propio account create y un token
+      #render json: @user, status: :ok
       json_response(response, :created)
+      
     end
   
     private
   
     def user_params
-      params.permit(
+      new_params=params.permit(
+        :rol,
         :nickname,
         :email,
+        :avatar,
+        :state,
+        :registrationdate,
         :password,
         :password_confirmation
       )
+      new_params[:rol]=@rol
+      return new_params
     end
   end
